cf5eecc6097736d7e349e9d34dd21b74f25c722f
Qarmau <105666478+Qarmau@users.noreply.github.com>
2024-08-09 16:34:26 +0300

    Delete assignment_project directory

--- assignment_project/assignments/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.assignment_list, name='assignment_list'),
-    path('download/<int:assignment_id>/', views.download_assignment, name='download_assignment'),
-]
